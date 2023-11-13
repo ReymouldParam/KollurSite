da5eac65b3af60264d9fa02fd58a5cb1e4b43464
ReymouldRevanth <revanth.danduboina@reymould.com>
2023-11-14 01:31:52 +0530

    Mobile responsive complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,26 @@ $(".gallery-carousel-main-container").slick({
     centerMode:true,
     centerPadding: '15%',
     dots: true,
-    focusOnSelect:true
+    focusOnSelect:true,
+    responsive: [
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 650,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
 
 $(".testimonials-carousel-main-container").slick({
